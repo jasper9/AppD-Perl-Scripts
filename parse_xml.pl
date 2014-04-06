8cae498a7795c39be175694fa3f83aaa57a3ff48
Josh Gray <jgray@vmware.com>
2014-04-05 21:42:21 -0600

    fixing xml parser

--- parse_xml.pl
+++ parse_xml.pl
@@ -16,13 +16,13 @@ ContentKey => '-content');
 #print $config->{photos}->{photo}->{'4662693906'}->{url_z};
 #print $config->{photos}->{photo}->{url_z};
 $n=0;
- while ( my ($key, $value) = each("%$config")) {
+ while ( my ($key, $value) = each(%$config)) {
 #        print "$key => $value\n";
-                while ( my ($key2, $value2) = each("%$value")) {
+                while ( my ($key2, $value2) = each(%$value)) {
                         #print "$key2 => $value2\n";
-                                while ( my ($key3, $value3) = each("%$value2")) {
+                                while ( my ($key3, $value3) = each(%$value2)) {
                                         #print "$key3 => $value3\n";
-                                                while ( my ($key4, $value4) = each("%$value3")) {
+                                                while ( my ($key4, $value4) = each(%$value3)) {
                                                         #print "$key4 => $value4\n";
                                                         if($key4 eq "url_z")
                                                         {
